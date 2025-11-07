4ad69f606dafed8669e67a8e0a620da1b8dd2f3e
Mateo RD <tu-correo@example.com>
2025-11-07 00:03:17 -0500

    primer commit, agregando el proyecto

---
+++ src/main/java/com/unimag/ahorroplusia/AhorroPlusIaApplication.java
@@ -0,0 +1,13 @@
+package com.unimag.ahorroplusia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhorroPlusIaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhorroPlusIaApplication.class, args);
+    }
+
+}
