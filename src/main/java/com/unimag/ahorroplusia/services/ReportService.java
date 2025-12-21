4282745ea67c41c8acf1940e3d2d4f67f111f182
Mateo RD <tu-correo@example.com>
2025-12-20 21:12:14 -0500

    quitando reporte

--- src/main/java/com/unimag/ahorroplusia/services/ReportService.java
+++
@@ -1,7 +0,0 @@
-package com.unimag.ahorroplusia.services;
-
-import com.unimag.ahorroplusia.dto.reports.FinancialReportResponse;
-
-public interface ReportService {
-    FinancialReportResponse getFinancialReport(Long userId, String period);
-}
